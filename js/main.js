8861214e0e3f605fc57a912250f4ee6d3910460b
hdlt <hectoradltc@gmail.com>
2022-05-25 20:14:13 -0500

    termine los problemas y agregue el ultimo

--- js/main.js
+++ js/main.js
@@ -2,20 +2,9 @@ let numero = prompt("dame un numero entre el 1 y el 100");
 let numero2 = prompt("dame otro numero entre el 1 y el 100");
 let numero3 = prompt("dame el ultimo numero entre el 1 y el 100");
 
-if ((numero>numero2) && (numero>numero3)) {
-    console.log(numero);
-}   else if ((numero<numero2) && (numero>numero3)) {
-    
-    console.log();
-}   else if ((numero>numero2) && (numero<numero3)) {
-    
-    console.log(numero3);
-} 
-    else  {
-        numero=numero2=numero3;
-        alert("no digas M****as usuario")
-}
-//////////////////////////////////////////////
+numero=parseFloat;
+numero2=parseFloat;
+numero3=parseFloat;
 
 // if ((numero>numero2) && (numero>numero3)) {
 //     console.log(numero);
@@ -28,34 +17,61 @@ if ((numero>numero2) && (numero>numero3)) {
 // } 
 //     else  {
 //         numero=numero2=numero3;
-//         alert("no digas M****as usuario")
+//         alert("Patata") 
 // }
+//////////////////////////////////////////////
+
+if ((numero<numero2) && (numero<numero3)) {
+    console.log(numero);
+}   else if ((numero>numero2) && (numero2<numero3)) {
+    console.log(numero2);
+}   else if ((numero3<numero2) && (numero>numero3)) {
+    console.log(numero3);
+}
+    else  {
+        numero=numero2=numero3;
+        alert("Patata")
+}
 /////////////////////////////////////////////////////////////////////////////
 // let x = prompt("dame un numero entre el 100 y el 200 para ver si es multiplo de 3");
 
-// if (x == (102, 105, 108, 111, 114, 117, 120, 123, 126, 129, 132, 135, 138, 141, 144, 147, 150, 153, 156, 159, 162, 165, 168, 171, 174, 177, 180, 183, 186, 189, 192, 195, 198) ) {
-//        console.log(x + " es multiplo de 3");
-// }
-// else if(x != (102, 105, 108, 111, 114, 117, 120, 123, 126, 129, 132, 135, 138, 141, 144, 147, 150, 153, 156, 159, 162, 165, 168, 171, 174, 177, 180, 183, 186, 189, 192, 195, 198)) {
-//     console.log(x + " patata")
+// x=parseInt(x);
+
+// if (x%3 == 0 ){
+//        alert(x + " es multiplo de 3");
 // }
 // else{
-//     console.log(x + " no es multiplo de 3");
+//     alert(x + " no es multiplo de 3");
 // }
-///////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////// listo
 // let n1 = prompt("dame un numero para ver si es suma de los siguentes");
 // let n2 = prompt("dame un numero");
 // let n3 = prompt("dame el ultimo numero");
 
+// n1=parseFloat(n1);
+// n2=parseFloat(n2);
+// n3=parseFloat(n3);
+
 // if (n1==(n2+n3)){
-//     alert(n1 + " el primero es suma de los otros 2");
+//     alert(n1 + " el primero es la suma de "+n2+" más "+n3  );
 // }
 // else if(n2==(n1+n3)){
-//     alert(n2 + " el segundo es suma de los otros 2");
+//     alert(n2 + " el segundo es la suma de "+n1+" más "+n3);
 // }
 // else if(n3==(n1+n2)){
-//     alert(n3 + " el tercero es suma de los otros 2");
+//     alert(n3 + " el tercero es la suma de "+n2+" más "+n1);
 // }
 // else{
 //     alert("mentiroso.....")
+// }
+//////////////////////////////////////////////////////
+
+// let n1 = prompt("dame un numero para ver si es par");
+
+// n1=parseFloat(n1);
+
+// if (n1%2==0){
+//     alert("El numero "+n1+" es par");
+// }else {
+//     alert("El numero "+n1+" es inpar");
 // }
